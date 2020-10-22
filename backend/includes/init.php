d98e7c96bc586339ac7a2d06c07407b7664d0aa6
Lucas Moraes <lucas.moraes88@yahoo.com>
2020-10-22 13:10:22 -0300

    Removemos a pasta backend

--- backend/includes/init.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-date_default_timezone_set('America/Sao_Paulo');
-
-define('BD_SERVIDOR', 'lucaspf777.mysql.uhserver.com');
-define('BD_USUARIO', 'lucaspf777');
-define('BD_SENHA', 'M@riad1988');
-define('BD_BANCO', 'lucaspf777');
--- backend/view/menu.php
+++
@@ -1,4 +0,0 @@
-<hr>
-<a href="index.php" class="btn btn-default">Inicio</a>
-<a href="cadastro.php" class="btn btn-success">Cadastar</a>
-<hr>
