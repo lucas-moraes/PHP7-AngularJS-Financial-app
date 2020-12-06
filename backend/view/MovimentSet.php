0a8c6013c66c9a81c61d0a2166c40d8490f0a791
Lucas Moraes <lucas.moraes88@yahoo.com>
2020-12-05 23:22:54 -0300

    Ajustes no backend

--- backend/view/MovimentSet.php
+++ backend/view/MovimentSet.php
@@ -4,4 +4,4 @@ require_once("../controller/ControllerSetMoviment.php");
 
 $insert = new EditMovController();
 
-$insert->editar($_POST['id'], $_POST['date'], $_POST['type'], $_POST['category'], $_POST['description'], $_POST['valor']);
+$insert->editar($_POST['id'], $_POST['date'], $_POST['type'], $_POST['category'], $_POST['description'], $_POST['value']);
