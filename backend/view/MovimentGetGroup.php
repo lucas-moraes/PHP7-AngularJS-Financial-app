0a4552c26f36826154fa82b7ceb052d095a1e2a8
Lucas Moraes <lucas.moraes88@yahoo.com>
2020-12-06 13:33:53 -0300

    Ajuste no JSON dos resumos

--- backend/view/MovimentGetGroup.php
+++ backend/view/MovimentGetGroup.php
@@ -8,8 +8,4 @@ $getGroupCategories = $list->getGroupCategories($_POST['month'], $_POST['year'])
 $getGroupCategoriesByYear = $list->getCategoriesByYear($_POST['year']);
 $getGroupMonth = $list->getGroutMonth($_POST['year']);
 
-echo json_encode($getGroupCategories);
-
-echo json_encode($getGroupCategoriesByYear);
-
-echo json_encode($getGroupMonth);
+echo json_encode($getGroupCategories + $getGroupCategoriesByYear + $getGroupMonth);
