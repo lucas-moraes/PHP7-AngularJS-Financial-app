7d5bc3946ee650aba0275ba75f9fcba49be01a5b
Lucas Moraes <lucas.moraes88@yahoo.com>
2020-12-14 13:52:41 -0300

    Login

---
+++ backend/includes/log.php
@@ -0,0 +1,8 @@
+<?php
+
+date_default_timezone_set('America/Sao_Paulo');
+
+define('BD_SERVIDOR', 'user7.mysql.uhserver.com');
+define('BD_USUARIO', 'lucasmoraes');
+define('BD_SENHA', 'M@riad88M@riad88');
+define('BD_BANCO', 'user7');
