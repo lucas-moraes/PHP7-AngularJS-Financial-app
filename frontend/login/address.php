5ab84908716dee26119bba6e52c167b94090a1a9
Lucas Moraes <lucas.moraes88@yahoo.com>
2020-12-14 13:52:41 -0300

    Login

--- frontend/login/address.php
+++
@@ -1,6 +0,0 @@
-<?php
-header ('Content-Type: application/json');
-
-$data['a1s542dq54yyusdfs823241sdfsdf'] = './encrypt.php';
-
-echo json_encode($data);
--- frontend/login/scripts.js
+++ frontend/login/scripts.js
@@ -1,7 +1,38 @@
+function logIn(){
+    const login = document.getElementById('login').value;
+    const pass = document.getElementById('pass').value;
 
-window.addEventListener('load', function () {
-    fetch('./address.php')
-        .then(res => {return res.json()})
-        .then(data => console.log(data))
-});
+    var formdata = new FormData();
+    formdata.append("pass", pass);
+
+    var requestOptions = {
+    method: 'POST',
+    body: formdata,
+    redirect: 'follow'
+    };
+
+    fetch("./encrypt.php", requestOptions)
+        .then(response => response.text())
+        .then(data => {
+            var formdata = new FormData();
+            formdata.append("login", login);
+            formdata.append("pass", data);
+            
+            var requestOptions = {
+              method: 'POST',
+              body: formdata,
+              redirect: 'follow'
+            };
+            
+            fetch("http://localhost/cloudcont/backend/view/Login.php", requestOptions)
+              .then(response => response.json())
+              .then(result => {
+                if(result.username == 'lucaspf'){
+                  window.location.href = "../home/principal.html";
+                } else {
+                  console.log("credenciais inválidas");
+                }
+              })
+        })
+}
 
