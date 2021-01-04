dd409e061e32524e29d79e3bbb880a0424962837
Lucas Moraes <lucas.moraes88@yahoo.com>
2021-01-04 13:38:14 -0300

    Ajuste  para o backend rodar na aws

--- backend/view/MovimentDel.php
+++ backend/view/MovimentDel.php
@@ -1,4 +1,7 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
 require_once("../controller/ControllerDelMoviment.php");
 
 $delete = new delMovimentController();
