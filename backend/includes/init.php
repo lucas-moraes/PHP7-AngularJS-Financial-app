420d9bc4c8ecadd6b32837d3af1b30ed5b38d557
Lucas Moraes <lucas.moraes88@yahoo.com>
2021-01-17 00:11:39 -0300

    Ajuste do endpoint do DB

--- backend/includes/init.php
+++ backend/includes/init.php
@@ -2,7 +2,7 @@
 
 date_default_timezone_set('America/Sao_Paulo');
 
-define('BD_SERVIDOR', 'cloudcont.c2uugizsybmz.us-east-2.rds.amazonaws.com');
-define('BD_USUARIO', 'admin');
-define('BD_SENHA', 'a1b2c3d4e5');
+define('BD_SERVIDOR', 'cloudcont.cvctkahbfzyk.sa-east-1.rds.amazonaws.com');
+define('BD_USUARIO', 'root');
+define('BD_SENHA', 'mariad88');
 define('BD_BANCO', 'lucaspf');
