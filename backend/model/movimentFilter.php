665b27e2d6beca0e8842c78f3c75109d42411b6b
Lucas Moraes <lucas.moraes88@yahoo.com>
2021-01-17 00:11:56 -0300

    Ajuste de consulta usando filtro

--- backend/model/movimentFilter.php
+++ backend/model/movimentFilter.php
@@ -21,8 +21,8 @@ class FilterMoviment
 
     public function filterMoviment($category, $month, $year)
     {
-        if (!empty($category) && !empty($month) && !empty($year)) {
-            $this->moviment = $this->mysqli->query("SELECT * FROM lc_movimento WHERE categoria = '$category' AND mes ='$month' AND ano='$year' ");
+        if ($category != 0 && $month == 0 && $year == 0) {
+            $this->moviment = $this->mysqli->query("SELECT * FROM lc_movimento WHERE categoria = '$category'");
             while ($row = $this->moviment->fetch_array(MYSQLI_ASSOC)) {
                 $cat = $row['categoria'];
                 $res = $this->mysqli->query("SELECT nome FROM categoria WHERE id='$cat'");
@@ -39,8 +39,8 @@ class FilterMoviment
                 $data['moviment'][] = array('id' => $row['id'], 'dia' => $row['dia'], 'mes' => $row['mes'], 'ano' => $row['ano'], 'tipo' => $row['tipo'], 'categoria' => $categoria, 'descricao' => $row['descricao'], 'valor' => $row['valor']);
             }
 
-            $entrada = $this->mysqli->query("SELECT SUM(valor) as entrada FROM lc_movimento WHERE categoria = '$category' AND mes ='$month' AND ano='$year' ");
-            $saida = $this->mysqli->query("SELECT SUM(valor*-1) as saida FROM lc_movimento WHERE categoria = '$category' AND mes ='$month' AND ano='$year' ");
+            $entrada = $this->mysqli->query("SELECT SUM(IF(valor > 0, valor, 0)) as entrada FROM lc_movimento WHERE categoria = '$category'");
+            $saida = $this->mysqli->query("SELECT SUM(valor < 0, valor, 0)*-1) as saida FROM lc_movimento WHERE categoria = '$category'");
             $in = $entrada->fetch_array(MYSQLI_ASSOC);
             $out = $saida->fetch_array(MYSQLI_ASSOC);
             $resultado = ($in['entrada'] - $out['saida']);
@@ -48,7 +48,7 @@ class FilterMoviment
             $data['total'] = $resultado;
 
             return $data;
-        } else if (empty($category) && !empty($month) && !empty($year)) {
+        } else if ($category == 0 && $month != 0 && $year != 0) {
             $this->moviment = $this->mysqli->query("SELECT * FROM lc_movimento WHERE mes ='$month' AND ano='$year' ");
             while ($row = $this->moviment->fetch_array(MYSQLI_ASSOC)) {
                 $cat = $row['categoria'];
@@ -66,8 +66,8 @@ class FilterMoviment
                 $data['moviment'][] = array('id' => $row['id'], 'dia' => $row['dia'], 'mes' => $row['mes'], 'ano' => $row['ano'], 'tipo' => $row['tipo'], 'categoria' => $categoria, 'descricao' => $row['descricao'], 'valor' => $row['valor']);
             }
 
-            $entrada = $this->mysqli->query("SELECT SUM(valor) as entrada FROM lc_movimento WHERE mes ='$month' AND ano='$year' ");
-            $saida = $this->mysqli->query("SELECT SUM(valor*-1) as saida FROM lc_movimento WHERE mes ='$month' AND ano='$year' ");
+            $entrada = $this->mysqli->query("SELECT SUM(if(valor > 0, valor, 0)) as entrada FROM lc_movimento WHERE mes ='$month' AND ano='$year' ");
+            $saida = $this->mysqli->query("SELECT SUM(if(valor < 0, valor, 0)*-1) as saida FROM lc_movimento WHERE mes ='$month' AND ano='$year' ");
             $in = $entrada->fetch_array(MYSQLI_ASSOC);
             $out = $saida->fetch_array(MYSQLI_ASSOC);
             $resultado = ($in['entrada'] - $out['saida']);
@@ -75,7 +75,34 @@ class FilterMoviment
             $data['total'] = $resultado;
 
             return $data;
-        } else if (empty($category) && empty($month) && !empty($year)) {
+        } else if ($category == 0 && $month != 0 && $year == 0) {
+            $this->moviment = $this->mysqli->query("SELECT * FROM lc_movimento WHERE categoria='$category' AND ano='$year'");
+            while ($row = $this->moviment->fetch_array(MYSQLI_ASSOC)) {
+                $cat = $row['categoria'];
+                $res = $this->mysqli->query("SELECT nome FROM categoria WHERE id='$cat'");
+                $row2 = mysqli_fetch_array($res);
+                $categoria = $row2['nome'];
+
+                if ($categoria === NULL) {
+                    switch ($cat) {
+                        case '777':
+                            $categoria = "Poupança";
+                            break;
+                    }
+                }
+                $data['moviment'][] = array('id' => $row['id'], 'dia' => $row['dia'], 'mes' => $row['mes'], 'ano' => $row['ano'], 'tipo' => $row['tipo'], 'categoria' => $categoria, 'descricao' => $row['descricao'], 'valor' => $row['valor']);
+            }
+
+            $entrada = $this->mysqli->query("SELECT SUM(IF(valor > 0, valor, 0)) as entrada FROM lc_movimento WHERE categoria='$category' AND ano='$year' ");
+            $saida = $this->mysqli->query("SELECT SUM(valor < 0, valor, 0)*-1) as saida FROM lc_movimento WHERE categoria='$category' AND ano='$year' ");
+            $in = $entrada->fetch_array(MYSQLI_ASSOC);
+            $out = $saida->fetch_array(MYSQLI_ASSOC);
+            $resultado = ($in['entrada'] - $out['saida']);
+
+            $data['total'] = $resultado;
+
+            return $data;
+        } else if ($category == 0 && $month == 0 && $year != 0) {
             $this->moviment = $this->mysqli->query("SELECT * FROM lc_movimento WHERE ano='$year' ");
             while ($row = $this->moviment->fetch_array(MYSQLI_ASSOC)) {
                 $cat = $row['categoria'];
@@ -93,8 +120,8 @@ class FilterMoviment
                 $data['moviment'][] = array('id' => $row['id'], 'dia' => $row['dia'], 'mes' => $row['mes'], 'ano' => $row['ano'], 'tipo' => $row['tipo'], 'categoria' => $categoria, 'descricao' => $row['descricao'], 'valor' => $row['valor']);
             }
 
-            $entrada = $this->mysqli->query("SELECT SUM(valor) as entrada FROM lc_movimento WHERE ano='$year' ");
-            $saida = $this->mysqli->query("SELECT SUM(valor*-1) as saida FROM lc_movimento WHERE ano='$year' ");
+            $entrada = $this->mysqli->query("SELECT SUM(IF(valor > 0, valor, 0)) as entrada FROM lc_movimento WHERE ano='$year' ");
+            $saida = $this->mysqli->query("SELECT SUM(IF(valor < 0, valor, 0)*-1) as saida FROM lc_movimento WHERE ano='$year' ");
             $in = $entrada->fetch_array(MYSQLI_ASSOC);
             $out = $saida->fetch_array(MYSQLI_ASSOC);
             $resultado = ($in['entrada'] - $out['saida']);
@@ -102,8 +129,8 @@ class FilterMoviment
             $data['total'] = $resultado;
 
             return $data;
-        } else if (!empty($category) && empty($month) && !empty($year)) {
-            $this->moviment = $this->mysqli->query("SELECT * FROM lc_movimento WHERE categoria = '$category' AND ano='$year' ");
+        } else if ($category != 0 && $month == 0 && $year == 0) {
+            $this->moviment = $this->mysqli->query("SELECT * FROM lc_movimento WHERE mes='$month' AND ano='$year' ");
             while ($row = $this->moviment->fetch_array(MYSQLI_ASSOC)) {
                 $cat = $row['categoria'];
                 $res = $this->mysqli->query("SELECT nome FROM categoria WHERE id='$cat'");
@@ -120,8 +147,35 @@ class FilterMoviment
                 $data['moviment'][] = array('id' => $row['id'], 'dia' => $row['dia'], 'mes' => $row['mes'], 'ano' => $row['ano'], 'tipo' => $row['tipo'], 'categoria' => $categoria, 'descricao' => $row['descricao'], 'valor' => $row['valor']);
             }
 
-            $entrada = $this->mysqli->query("SELECT SUM(valor) as entrada FROM lc_movimento WHERE categoria = '$category' AND ano='$year' ");
-            $saida = $this->mysqli->query("SELECT SUM(valor*-1) as saida FROM lc_movimento WHERE categoria = '$category' AND ano='$year' ");
+            $entrada = $this->mysqli->query("SELECT SUM(IF(valor > 0, valor, 0)) as entrada FROM lc_movimento WHERE mes='$month' AND ano='$year' ");
+            $saida = $this->mysqli->query("SELECT SUM(IF(valor < 0, valor, 0)*-1) as saida FROM lc_movimento WHERE mes='$month' AND ano='$year' ");
+            $in = $entrada->fetch_array(MYSQLI_ASSOC);
+            $out = $saida->fetch_array(MYSQLI_ASSOC);
+            $resultado = ($in['entrada'] - $out['saida']);
+
+            $data['total'] = $resultado;
+
+            return $data;
+        } else if ($category != 0 && $month != 0 && $year == 0) {
+            $this->moviment = $this->mysqli->query("SELECT * FROM lc_movimento WHERE categoria='$category' AND mes='$month'");
+            while ($row = $this->moviment->fetch_array(MYSQLI_ASSOC)) {
+                $cat = $row['categoria'];
+                $res = $this->mysqli->query("SELECT nome FROM categoria WHERE id='$cat'");
+                $row2 = mysqli_fetch_array($res);
+                $categoria = $row2['nome'];
+
+                if ($categoria === NULL) {
+                    switch ($cat) {
+                        case '777':
+                            $categoria = "Poupança";
+                            break;
+                    }
+                }
+                $data['moviment'][] = array('id' => $row['id'], 'dia' => $row['dia'], 'mes' => $row['mes'], 'ano' => $row['ano'], 'tipo' => $row['tipo'], 'categoria' => $categoria, 'descricao' => $row['descricao'], 'valor' => $row['valor']);
+            }
+
+            $entrada = $this->mysqli->query("SELECT SUM(IF(valor > 0, valor, 0)) as entrada FROM lc_movimento WHERE categoria='$category' AND mes='$month'");
+            $saida = $this->mysqli->query("SELECT SUM(IF(valor < 0, valor, 0)*-1) as saida FROM lc_movimento WHERE categoria='$category' AND mes='$month'");
             $in = $entrada->fetch_array(MYSQLI_ASSOC);
             $out = $saida->fetch_array(MYSQLI_ASSOC);
             $resultado = ($in['entrada'] - $out['saida']);
