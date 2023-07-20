52c133e1b8e6013069d268058bc5ae1b2bce0531
Lucas Moraes <lucas.moraes88@yahoo.com>
2023-07-20 15:46:56 -0300

    Delete init.php

--- backend/includes/init.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-date_default_timezone_set('America/Sao_Paulo');
-
-define('BD_SERVIDOR', 'cloudcont.cvctkahbfzyk.sa-east-1.rds.amazonaws.com');
-define('BD_USUARIO', 'root');
-define('BD_SENHA', 'mariad88');
-define('BD_BANCO', 'lucaspf');
